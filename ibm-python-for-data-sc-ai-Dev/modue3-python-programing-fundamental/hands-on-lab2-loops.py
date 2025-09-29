8adb0afac62166dcc9ad6a0f6d1a8609e7508002
Safaa Serieddine <serieddine.s.r@gmail.com>
2025-09-29 06:44:49 +0300

    Finish the hands-on-lab loops

--- ibm-python-for-data-sc-ai-Dev/modue3-python-programing-fundamental/hands-on-lab2-loops.py
+++ ibm-python-for-data-sc-ai-Dev/modue3-python-programing-fundamental/hands-on-lab2-loops.py
@@ -35,6 +35,50 @@ for i, square in enumerate(squares):
     print(i, square)    
 
 # ** While Loops**
+# The while loop is used to repeat a block of code as long as a certain condition is true.
+# For example, we can use a while loop to print the elements in the previous dates list except 1973:
+dates = [1982,1980,1973]
+i = 0
+while dates[i] != 1973:
+    print(dates[i])
+    i = i + 1
+print('It took ', i ,'repetitions to get out of loop.')
 
+# Quiz on Loops
+# Write a for loop that prints out all the elements between -5 and 5 using the range function.
+for i in range(-5,6):
+    print(i)
+# should print: -5 -4 -3 -2 -1 0 1 2 3 4 5
 
+# Print the elements of the following list: Genres=[ 'rock', 'R&B', 'Soundtrack', 'R&B', 'soul', 'pop']
+Genres=['rock', 'R&B', 'Soundtrack', 'R&B', 'soul', 'pop']
+for genre in Genres:
+    print(genre) # should print: rock R&B Soundtrack R&B soul pop
 
+# Write a for loop that prints out the following list: 
+# squares=['red', 'yellow', 'green', 'purple', 'blue']
+squares=['red', 'yellow', 'green', 'purple', 'blue']
+for square in squares:
+    print(square)
+# should print: red yellow green purple blue
+
+# Write a while loop to display the values of the Rating of an album playlist stored in 
+# thePlayListRatings list. If the score is less than 6, exit the loop.
+# The list PlayListRatings is given by: PlayListRatings = [10, 9.5, 10, 8, 7.5, 5, 10, 10]
+PlayListRatings = [10, 9.5, 10, 8, 7.5, 5, 10, 10]
+i =0
+while PlayListRatings[i] >= 6:
+    print(PlayListRatings[i])
+    i = i + 1
+# should print: 10 9.5 10 8 7.5
+
+# Write a while loop to copy the strings 'orange' of the list squares to the list new_squares. 
+# Stop and exit the loop if the value on the list is not 'orange':
+squares = ['orange', 'orange', 'purple', 'blue ', 'orange']
+new_squares = []
+i = 0
+while i < len(squares) and squares[i] =="orange":
+    new_squares.append(squares[i])
+    i = i + 1
+print(new_squares)
+# should print: ['orange', 'orange']
