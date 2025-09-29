bf39f467672e2199d8a13e9b559f7831e02c35af
Safaa Serieddine <serieddine.s.r@gmail.com>
2025-09-29 14:13:42 +0300

    Continue lab

---
+++ ibm-python-for-data-sc-ai-Dev/modue3-python-programing-fundamental/4-exception-handling-hands-on-lab.py
@@ -0,0 +1,8 @@
+# Exception Hnaling Hands-on Lab
+
+# What is an Exception?
+# An exception is an event that occurs during the execution of a program that disrupts the
+# normal flow of the program's instructions.
+# When an exception occurs, the program stops executing and raises an error message.
+
+# Run each piece of code and observe the exception raised
