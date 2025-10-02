f9b6f1a5c222cc14e868546af1905ebd5aff2159
Safaa Serieddine <serieddine.s.r@gmail.com>
2025-10-03 02:23:08 +0300

    Add HTTp and Request hands on lab

---
+++ ibm-python-for-data-sc-ai-Dev/module5-API-and-data-collection/2-http-and-request-hands-on-lab.py
@@ -0,0 +1,12 @@
+# HTTP and Requests
+
+# Objective: 
+# 1- Understand HTTP
+# 2- Handle HTTP Requests
+
+# Uniform Resource Locator:URL
+# Uniform resource locator (URL) is the most popular way to find resources on the web.
+#  We can break the URL into three parts.
+# 1-Scheme: This is this protocol, for this lab it will always be http://
+# 2-Internet address  This will be used to find the location here are some examples: www.ibm.com and  www.gitlab.com 
+# 3-Route:- Location on the web server for example: /images/IDSNlogo.png
