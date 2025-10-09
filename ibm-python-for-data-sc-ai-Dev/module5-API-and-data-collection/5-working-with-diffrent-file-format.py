720bd679c6b1187d04b2e48c70bc29b6da600abb
Safaa Serieddine <serieddine.s.r@gmail.com>
2025-10-09 12:32:03 +0300

    Working on hands-on lab 5

--- ibm-python-for-data-sc-ai-Dev/module5-API-and-data-collection/5-working-with-diffrent-file-format.py
+++ ibm-python-for-data-sc-ai-Dev/module5-API-and-data-collection/5-working-with-diffrent-file-format.py
@@ -19,6 +19,14 @@
 # 3- Load - Loading the data inside a data warehouse. Data warehouse essentially contains large volumes of data that are accessed to gather insights.
 
 # 3- Working with different file formats
-#In the real-world, people rarely get neat tabular data. Thus, it is mandatory for any data scientist (or data engineer) to be aware of 
-# different file formats, common challenges in handling them and the best, most efficient ways to handle this data in real life. 
-# We have reviewed some of this content in other modules.
+#In the real-world, people rarely get neat tabular data. Thus, it is mandatory for any data scientist
+# (or data engineer) to be aware of different file formats, common challenges in handling them and 
+# the best, most efficient ways to handle this data in real life. 
+
+# A file format is a standardized method for encoding information to be stored in a computer file.
+
+# 1- It specifies whether the file is binary or ASCII (text-based).
+
+3 2-It dictates how the information is organized within the file (e.g., the Comma-Separated Values, or CSV format, organizes tabular data as plain text).
+
+The file format can typically be identified by its file extension (e.g., a file named Data.csv indicates the CSV format).
