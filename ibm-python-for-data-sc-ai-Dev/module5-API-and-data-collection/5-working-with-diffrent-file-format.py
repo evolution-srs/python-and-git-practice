4da2b2f4b490b79d8f9f79cfa7e3e613e54ba0a9
Safaa Serieddine <serieddine.s.r@gmail.com>
2025-10-09 12:12:53 +0300

    Add Jupyter notebook for the fifth lab

---
+++ ibm-python-for-data-sc-ai-Dev/module5-API-and-data-collection/5-working-with-diffrent-file-format.py
@@ -0,0 +1,24 @@
+# Working with different file formats 
+# Objective
+# 1- Data Engineering
+# 2- Data Engineering Process
+# 3- Working with different file formats
+# 4- Data Analysis
+
+# 1- Data Engineering: is s one of the most critical and foundational skills in any data scientist’s toolkit.
+
+# 2- Data Engineering Process
+# There are several steps in Data Engineering process:
+# 1- Extract - Data extraction is getting data from multiple sources. 
+# Ex. Data extraction from a website using Web scraping or gathering information from the data that are stored in different formats
+# (JSON, CSV, XLSX etc.).
+
+# 2- Transform - Transforming the data means removing the data that we don't need for further analysis and converting the data in the format 
+# that all the data from the multiple sources is in the same format.
+
+# 3- Load - Loading the data inside a data warehouse. Data warehouse essentially contains large volumes of data that are accessed to gather insights.
+
+# 3- Working with different file formats
+#In the real-world, people rarely get neat tabular data. Thus, it is mandatory for any data scientist (or data engineer) to be aware of 
+# different file formats, common challenges in handling them and the best, most efficient ways to handle this data in real life. 
+# We have reviewed some of this content in other modules.
